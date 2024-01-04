da09456ed475d5ebcc8aa593d891943f1da02d9f
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2024-01-04 20:49:11 +1000

    add and fix tags

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,12 +5,12 @@ const postsCollection = defineCollection({
       title: z.string(),
       pubDate: z.date(),
       description: z.string(),
-    //   author: z.string(),
+      // author: z.string(),
       image: z.object({
         url: z.string(),
         alt: z.string()
       }),
-    //   tags: z.array(z.string())
+      tags: z.array(z.string())
     })
  });
 
