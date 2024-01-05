0ffa91f06e9d9926cbb92125b19c2dd8b27fe08a
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2024-01-05 23:01:42 +1000

    add support for Astro/Image in slugs and blog page

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,15 @@
 import { defineCollection, z } from "astro:content";
 
 const postsCollection = defineCollection({
-    schema: z.object({
+  schema: ({ image }) => z.object({
       title: z.string(),
       pubDate: z.date(),
       description: z.string(),
       author: z.optional(z.string()),
-      image: z.object({
-        url: z.string(),
-        alt: z.string()
+      image: image().refine((img) => img.width >= 600, {
+        message: "Cover image must be at least 600 pixels wide!",
       }),
+      imageAlt: z.string(),
       tags: z.array(z.string()),
       // Add a 'draft' property, which is a boolean false or true no ``
       draft: z.optional(z.boolean()),
